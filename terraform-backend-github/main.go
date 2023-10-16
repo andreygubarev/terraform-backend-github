f264b3ae32f23ee9ce1b97616316860f5c65c46e
Andrey Gubarev <andrey@andreygubarev.com>
2023-10-16 22:34:25 +0400

    added gin framework and http route handling in terraform backend service

--- terraform-backend-github/main.go
+++ terraform-backend-github/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "log"
+import "github.com/gin-gonic/gin"
 
 func main() {
-	log.Println("terraform-backend-github")
+	r := gin.Default()
+	r.Any("/:org/:repo/*path", get)
+	r.Run(":8080")
+}
+
+func get(c *gin.Context) {
+	org := c.Param("org")
+	repo := c.Param("repo")
+	path := c.Param("path")
+
+	if path == "/" {
+		c.JSON(400, gin.H{
+			"error": "path is required",
+		})
+	}
+
+	ref := c.Query("ref")
+	if ref == "" {
+		ref = "main"
+	}
+
+	c.JSON(200, gin.H{
+		"org":  org,
+		"repo": repo,
+		"path": path,
+	})
 }
