e79c4f61d7106b4b73ecd6a28112eaee706f9e3e
Andrey Gubarev <andrey@andreygubarev.com>
2023-10-17 11:03:17 +0400

    added github api integration to fetch repository contents

--- terraform-backend-github/main.go
+++ terraform-backend-github/main.go
@@ -1,16 +1,26 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/go-github/v56/github"
+)
+
+var gh *github.Client
 
 func main() {
+	token := os.Getenv("GITHUB_TOKEN")
+	gh = github.NewClient(nil).WithAuthToken(token)
+
 	r := gin.Default()
-	r.Any("/:org/:repo/*path", get)
+	r.GET("/:owner/:repo/*path", get)
 	r.Run(":8080")
 }
 
 func get(c *gin.Context) {
-	org := c.Param("org")
-	repo := c.Param("repo")
+	owner := c.Param("owner")
+	repoName := c.Param("repo")
 	path := c.Param("path")
 
 	if path == "/" {
@@ -24,9 +34,32 @@ func get(c *gin.Context) {
 		ref = "main"
 	}
 
-	c.JSON(200, gin.H{
-		"org":  org,
-		"repo": repo,
-		"path": path,
+	_, _, err := gh.Repositories.Get(c, owner, repoName)
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	fileContent, _, _, err := gh.Repositories.GetContents(c, owner, repoName, path, &github.RepositoryContentGetOptions{
+		Ref: ref,
 	})
+
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	content, err := fileContent.GetContent()
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	c.Data(200, "application/json", []byte(content))
 }
