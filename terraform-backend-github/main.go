239a7c214136b157abc153a91532c9c3952daf57
Andrey Gubarev <andrey@andreygubarev.com>
2023-10-17 16:39:19 +0400

    added lock and unlock http methods to terraform backend with error handling

--- terraform-backend-github/main.go
+++ terraform-backend-github/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
@@ -20,6 +21,8 @@ func main() {
 	r.GET("/:owner/:repo/*path", getHandler)
 	r.POST("/:owner/:repo/*path", postHandler)
 	r.DELETE("/:owner/:repo/*path", deleteHandler)
+	r.Handle("LOCK", "/:owner/:repo/*path", lockHandler)
+	r.Handle("UNLOCK", "/:owner/:repo/*path", deleteHandler)
 	r.Run(":8080")
 }
 
@@ -155,4 +158,59 @@ func deleteHandler(c *gin.Context) {
 		return
 	}
 	c.Data(200, "application/json", []byte("{}"))
+}
+
+func lockHandler(c *gin.Context) {
+	state, err := NewTerraformState(c)
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	fileContent, exists, err := state.Content(c)
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	body, err := c.GetRawData()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	if exists {
+		content, err := fileContent.GetContent()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		c.Data(http.StatusConflict, "application/json", []byte(content))
+		return
+	} else {
+		_, _, err = gh.Repositories.CreateFile(c, state.Owner, state.Repo, state.Path, &github.RepositoryContentFileOptions{
+			Message: github.String("create terraform state lock"),
+			Content: body,
+			Branch:  github.String(state.Ref),
+		})
+
+		if err != nil {
+			c.JSON(400, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		c.Data(200, "application/json", body)
+		return
+	}
 }
